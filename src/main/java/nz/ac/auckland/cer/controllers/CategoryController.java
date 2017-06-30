4006cd76b70832cd96e1f4a5783ec06352d027aa
Jamie Diprose <jamie.diprose@gmail.com>
2017-06-30 14:18:52 +1200

    Create an API call for getting the list of roles

--- src/main/java/nz/ac/auckland/cer/controllers/CategoryController.java
+++ src/main/java/nz/ac/auckland/cer/controllers/CategoryController.java
@@ -3,8 +3,10 @@ package nz.ac.auckland.cer.controllers;
 
 import nz.ac.auckland.cer.model.categories.ContentType;
 import nz.ac.auckland.cer.model.categories.ResearchPhase;
+import nz.ac.auckland.cer.model.categories.Role;
 import nz.ac.auckland.cer.repository.ResearchPhaseRepository;
 import nz.ac.auckland.cer.repository.ContentTypeRepository;
+import nz.ac.auckland.cer.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,21 +27,31 @@ public class CategoryController extends AbstractController {
     @Autowired
     private ResearchPhaseRepository researchPhaseRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public CategoryController() {
         super();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/category/contentType")
     public ResponseEntity<String> getContentTypes() {
-        final List<ContentType> contentTypes = contentTypeRepository.findAll();
-        String results = this.getFilteredResults(contentTypes, ContentType.ENTITY_NAME, "contentItems");
+        final List<ContentType> items = contentTypeRepository.findAll();
+        String results = this.getFilteredResults(items, ContentType.ENTITY_NAME, "contentItems");
         return new ResponseEntity<>(results, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/category/researchPhase")
     public ResponseEntity<String> getResearchPhases() {
-        final List<ResearchPhase> contentTypes = researchPhaseRepository.findAll();
-        String results = this.getFilteredResults(contentTypes, ResearchPhase.ENTITY_NAME, "contentItems");
+        final List<ResearchPhase> items = researchPhaseRepository.findAll();
+        String results = this.getFilteredResults(items, ResearchPhase.ENTITY_NAME, "contentItems");
+        return new ResponseEntity<>(results, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/category/role")
+    public ResponseEntity<String> getRole() {
+        final List<Role> items = roleRepository.findAll();
+        String results = this.getFilteredResults(items, Role.ENTITY_NAME, "contentRoles");
         return new ResponseEntity<>(results, HttpStatus.OK);
     }
 }
