ea5deebfe65f66edf1fe16cbc816e54af7c30f4d
Jamie Diprose <jamie.diprose@gmail.com>
2017-07-04 13:31:52 +1200

    Rename

--- src/main/java/nz/ac/auckland/cer/config/StaticResourceConfig.java
+++ src/main/java/nz/ac/auckland/cer/config/StaticResourceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+public class StaticResourceConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("file:/Users/jamie/test/");
