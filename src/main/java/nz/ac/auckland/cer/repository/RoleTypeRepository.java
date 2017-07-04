a9773ac23373172dd878df99e97af29bfa74c45f
Jamie Diprose <jamie.diprose@gmail.com>
2017-07-04 14:08:36 +1200

    Refactor Role into RoleType

---
+++ src/main/java/nz/ac/auckland/cer/repository/RoleTypeRepository.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.cer.repository;
+
+import nz.ac.auckland.cer.model.categories.RoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleTypeRepository extends JpaRepository<RoleType, Integer> {
+}
