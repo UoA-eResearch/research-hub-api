f445c2a5bd3d97791ae59502e079ea776c8cd1b2
Jamie Diprose <jamie.diprose@gmail.com>
2017-08-16 15:24:16 +1200

    Enable location of static resources to be configured via application.properties

--- src/main/java/nz/ac/auckland/cer/config/StaticResourceConfig.java
+++ src/main/java/nz/ac/auckland/cer/config/StaticResourceConfig.java
@@ -1,14 +1,18 @@
 package nz.ac.auckland.cer.config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class StaticResourceConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${app.assets-location}")
+    private String assetsLocation;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("file:/Users/jamie/test/");
+        registry.addResourceHandler("/**").addResourceLocations(assetsLocation);
     }
 }
