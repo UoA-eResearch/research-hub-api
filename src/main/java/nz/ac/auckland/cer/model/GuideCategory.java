825fb9edefd6f61e58c8f8238ebddcaef091f554
Jamie Diprose <jamie.diprose@gmail.com>
2017-08-31 16:14:01 +1200

    Fix error with join (id's wrong way around)

--- src/main/java/nz/ac/auckland/cer/model/GuideCategory.java
+++ src/main/java/nz/ac/auckland/cer/model/GuideCategory.java
@@ -31,8 +31,8 @@ public class GuideCategory {
 
     @JsonFilter("contentItems")
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "guide_category_content", joinColumns = @JoinColumn(name = "content_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "guide_category_id", referencedColumnName = "id"))
-    @JsonIgnoreProperties(value = {"contentItems", "similarContentItems"}, allowSetters=true)
+    @JoinTable(name = "guide_category_content", joinColumns = @JoinColumn(name = "guide_category_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "content_id", referencedColumnName = "id"))
+//    @JsonIgnoreProperties(value = {"contentItems", "similarContentItems"}, allowSetters=true)
     private Set<Content> contentItems;
 
     public int getId() {
