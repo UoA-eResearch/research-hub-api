a2d1d09965b924a3b3a078e7fd962eeaa865613c
Jamie Diprose <jamie.diprose@gmail.com>
2017-11-22 15:27:37 +1300

    Create ApiInfo from information within the POM

--- src/main/java/nz/ac/auckland/cer/config/SwaggerConfig.java
+++ src/main/java/nz/ac/auckland/cer/config/SwaggerConfig.java
@@ -1,20 +1,35 @@
 package nz.ac.auckland.cer.config;
 
+import org.apache.maven.model.Contributor;
+import org.apache.maven.model.License;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.FileReader;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -27,13 +42,43 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "The Research Hub API",
-                "A RESTful API for the Research Hub",
-                "0.1.0",
-                "",
-                "James Diprose",
-                "MIT",
-                "https://raw.githubusercontent.com/UoA-eResearch/research-hub-api/mvp/LICENSE");
+        String title = "";
+        String description = "";
+        String version = "";
+        String termsOfServiceUrl = "";
+        Contact contact = new Contact("", "", "");
+        String license = "";
+        String licenseUrl = "";
+        Collection<VendorExtension> vendorExtensions = new ArrayList<>();
+
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader("pom.xml"));
+
+            title = model.getName();
+            description = model.getDescription();
+            version = model.getVersion();
+            termsOfServiceUrl = "";
+
+            List<Contributor> contributors = model.getContributors();
+
+            if (contributors.size() > 0) {
+                Contributor contributor = contributors.get(0);
+                contact = new Contact(contributor.getName(), contributor.getUrl(), contributor.getEmail());
+            }
+
+            List<License> licenses = model.getLicenses();
+
+            if (licenses.size() > 0) {
+                License firstLicense = licenses.get(0);
+                license = firstLicense.getName();
+                licenseUrl = firstLicense.getUrl();
+            }
+        }
+        catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        return new ApiInfo(title, description, version, termsOfServiceUrl, contact, license, licenseUrl, vendorExtensions);
     }
 }
