683105d0e35a72fa5a3f16db6e0761a252fc1386
Noel <noel.zeng@auckland.ac.nz>
2019-01-07 17:21:19 +1300

    Add produces annotation manually to each endpoint - Spring Boot used to offer the setting in application properties to specify a charset for all endpoints. However, they have now changed its behaviour (see https://github.com/spring-projects/spring-boot/issues/5459) - I have now added an annotation to each endpoint type so each endpoint uses UTF-8 charset. - If we have an endpoint that returns images or other binary data, the produces value needs to be overridden. See https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/RequestMapping.html#produces--

--- src/main/java/nz/ac/auckland/cer/controllers/PersonController.java
+++ src/main/java/nz/ac/auckland/cer/controllers/PersonController.java
@@ -19,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -27,6 +28,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping(produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Api(tags = {"Person"}, description = "Operations on person")
 public class PersonController extends AbstractSearchController {
 
