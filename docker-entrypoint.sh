3bf9d7caa6ab8244de6af8859878d3c6cc9e810a
Noel <noel.zeng@auckland.ac.nz>
2019-05-22 17:52:08 +1200

    fix running as user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,14 @@
 
 if [ "$1" == "--local" ]; then
     # If the local flag is passed, we run a version of the rh api project set up for development.
-    # Change the permission for the target folder.
-    chown -R rhapi-user /research-hub-api/target/
     # Run as user with uid matching the host user, so generated directories and classfiles
     # can be accessed/changed by host.
-    su - rhapi-user
-    mvn dependency:go-offline
+    # Change the permission for the target folder.
+    chown -R rhapi-user /research-hub-api/target/
+    su rhapi-user -c 'mvn dependency:go-offline'
     # Run a version of spring boot which watches the /target directory, and reloads when
     # generated classfiles change.
-    mvn spring-boot:run -Drun.jvmArguments=-Dspring.config.location=/application.properties
+    su rhapi-user -c 'mvn spring-boot:run -Drun.jvmArguments=-Dspring.config.location=/application.properties'
     exit
 fi
 
